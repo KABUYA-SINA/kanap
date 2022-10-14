f288aef20445edef9f8b3997a8409d01ee6dfd16
KABUYA <kabuya-sina@netcourrier.com>
2022-10-14 13:57:06 +0200

    script tryin'

--- front/js/product.js
+++ front/js/product.js
@@ -8,4 +8,16 @@ const allOptionValues = document.getElementById('colors');
 
 // all quantity products
 
-const allQuantityObjets = document.getElementById('quantity'); 
+const allQuantityObjets = document.getElementById('quantity');
+
+let titleProducts = document.getElementById('title');
+let productDescriptions = document.getElementById('description');
+
+let clickArticles = document.getElementsByTagName('a');
+clickArticles.addEventListener('click', function (event) {
+    let allproductNames = document.getElementsByClassName('productName');
+    allproductNames.addEventListener('change'), function (event) {
+        let titleProducts = document.getElementById('title');
+        titleProducts.innerText = e.target.value;
+    }
+});
--- front/js/script.js
+++ front/js/script.js
@@ -1,3 +1,7 @@
 //variables
 
 const allProducts = document.getElementById('items'); 
+
+//by class
+let allproductNames = document.getElementsByClassName('productName');
+let allproductDescriptions = document.getElementsByClassName('productDescription');
