01e8dce4992f9d6756d2decf53f4da68262240d8
KABUYA <kabuya-sina@netcourrier.com>
2022-10-14 12:49:47 +0200

    options and products page modified

---
+++ front/js/product.js
@@ -0,0 +1,11 @@
+// variables
+
+
+// all value option 
+const allOptionValues = document.getElementById('colors');
+
+
+
+// all quantity products
+
+const allQuantityObjets = document.getElementById('quantity'); 
