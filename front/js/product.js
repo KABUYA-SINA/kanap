90ada23df67f2420c8fdec0b845f2cf6d85b65f1
KABUYA <kabuya-sina@netcourrier.com>
2022-10-14 19:02:58 +0200

    scripts

--- front/js/product.js
+++ front/js/product.js
@@ -4,20 +4,16 @@
 // all value option 
 const allOptionValues = document.getElementById('colors');
 
-
-
 // all quantity products
 
 const allQuantityObjets = document.getElementById('quantity');
 
 let titleProducts = document.getElementById('title');
+//let firstTitleProduct = titleProducts[0]; 
 let productDescriptions = document.getElementById('description');
 
-let clickArticles = document.getElementsByTagName('a');
-clickArticles.addEventListener('click', function (event) {
-    let allproductNames = document.getElementsByClassName('productName');
-    allproductNames.addEventListener('click'), function (event) {
-        let titleProducts = document.getElementById('title');
-        titleProducts.innerText = e.target.value;
-    }
-});
+
+
+
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,52 @@
 //variables
 
-const allProducts = document.getElementById('items'); 
-
 //by class
-let allproductNames = document.getElementsByClassName('productName');
+//let allproductNames = document.getElementsByClassName('productName');
 let allproductDescriptions = document.getElementsByClassName('productDescription');
+
+
+    //const links = document.getElementsByTagName('a');
+    // const allProducts = document.getElementById('items');
+    // let clickArticles = document.getElementsByTagName('article'); 
+    // let titleProducts = document.getElementById('title');
+    // const links = document.querySelector('#items a > article');  
+    //       links.addEventListener('click', function (event) {
+    //           let clickArticles = document.getElementsByTagName('article');
+    //           for (let clickArticle of clickArticles)
+    //               clickArticle.addEventListener('click', function (event) {
+    //                   let titleProducts = document.getElementById('title');
+    //                   // let allproductNames = document.getElementsByClassName('productName');
+    //                   titleProducts.innerHTML = event.target.value;
+    //               });
+    //       });
+
+//console.log(allproductNames);
+//console.log(clickArticles);
+//console.log(titleProducts); 
+//console.log(links.childNodes);
+
+function requestVariables() {
+    fetch('http://127.0.0.1:5500/front/html/product.html?id=42')
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+
+        })
+        .then(function (value) {
+            let titleProducts = document.getElementById('title');
+            titleProducts.innerText = value.queryString.id;
+        })
+        .catch(function (err) {
+            
+        });
+}
+   let allproductNames = document.getElementsByClassName('productName')
+   for (let allproductName of allproductNames)
+      allproductName.addEventListener('click', function requestVariables (){
+           let titleProducts = document.getElementById('title');
+           titleProducts.innerHTML = requestVariables.target.value;
+       });
+//console.log(allproductNames); 
+
+
