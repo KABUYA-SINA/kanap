a4656e0455741f74940e83496b247b19992ef16f
KABUYA <kabuya-sina@netcourrier.com>
2022-10-17 17:06:55 +0200

    ideas scrpits undone

--- front/js/product.js
+++ front/js/product.js
@@ -1,35 +1,119 @@
 // variables
+let urlParams = (new URL(location)).searchParams;
+let productId = urlParams.get('id'); 
+
+fetch('http://localhost:3000/api/products/'+productId)
+    .then(function (res) {
+        if (res.ok) {
+            res.json()
+                .then(function (product) {
+                    let titleProducts = document.getElementById('title');
+                    titleProducts.innerHTML = product.name;
+                    
+                    let images = document.getElementsByClassName('item__img');
+                    for (let image of images)
+                      image.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">` 
+
+                    let prices = document.getElementById('price'); 
+                    prices.innerHTML = product.price;
+
+                    let productDescriptions = document.getElementById('description');
+                    productDescriptions.innerHTML = product.description; 
+
+
+                    
+
+
+
+                    // let selects = document.getElementsByTagName('select'); 
+                    // for (let select of selects){
+                    //     select.addEventListener('click', function (event){
+                    //         event.preventDefault();
+                    //         const allOptionValues = document.getElementById('colors')
+                    //         for (let allOptionValue of allOptionValues) {
+                    //         allOptionValues.addEventListener('click', function(){
+                    //             allOptionValues.innerHTML = product.colors;
+                    //             console.log(select); 
+                    //         })
+                    //     }}); 
+                    // }
+
+                    // const allOptionValues = document.getElementById('colors');
+                    // allOptionValues.addEventListener('click', function(){
+                    //     for (let allOptionValue of allOptionValues){
+                    //         allOptionValue.addEventListener('change', function(){
+                    //             let options = document.getElementsByTagName('option')
+                    //             for (let i = 0; i < options.length; i++) {
+                    //                 let option = options[i];
+                    //                 option.innerHTML = product.colors;
+                    //             }
+                    //         });
+
+                    //     }
+ 
+                    // }); 
+
+
+
+
+
+
+
+
+                        // for (let allOptionValue of allOptionValues){
+                        //     allOptionValue.addEventlistener('click', function(){
+                        //         let options = document.getElementsByTagName('option')
+                        //         for (let i = 0; i < options.length; i++) {
+                        //             let option = options[i];
+                        //             option.innerHTML = product.colors;
+                        //             console.log(option);
+                        //         }
+                        //     });
+                        // }
+                            
+
+
+
+                        //    function onClick (e){
+                        //        let options = document.getElementsByTagName('option')
+                        //        for (let i = 0; i < options.length; i++) {
+                        //            let option = options[i];
+                        //            option.innerHTML = product.colors;
+                        //            console.log(option);
+                        //        }
+                        //    }
+                        
+
+
+
+
+
+                        //for (let i = 0; i < allOptionValues; i++)
+                        //allOptionValue.innerHTML = product.colors;
+                        //i.innerHTML = product.colors;
+                    // for (let allOptionValue of allOptionValues)
+                    //     allOptionValue.innerHTML = product.colors;
+                    // allOptionValues.addEventListener('click', function(e){
+                    //     allOptionValues.innerHTML = product.colors; //e.target.value;// product.colors; 
+                    // });
+
+
+
+                    
+                    console.log(product); 
+                    console.log(images);
+                    //console.log(allOptionValues); 
+                }); 
+
+        }
+
+    })
 
 
 // all value option 
-const allOptionValues = document.getElementById('colors');
-
-// all quantity products
 
 const allQuantityObjets = document.getElementById('quantity');
-
-let titleProducts = document.getElementById('title');
-//let firstTitleProduct = titleProducts[0]; 
-let productDescriptions = document.getElementById('description');
+// all quantity products
 
 
-function requestVariables() {
-    fetch('http://127.0.0.1:5500/front/html/product.html?id=42')
-        .then(function (res) {
-            if (res.ok) {
-                return res.json();
-            }
-
-        })
-        .then(function (value) {
-            let titleProducts = document.getElementById('title');
-            titleProducts.innerText = value.postData.text; //value.target.value; //value.queryString.value; //value.target.value //value.postData.text;
-        })
-        .catch(function (err) {
-            
-        });
-    }
-
-
-titleProducts.addEventListener('change', requestVariables); 
 
--- front/js/script.js
+++ front/js/script.js
@@ -1,89 +1,24 @@
 //variables
+const allProducts = document.getElementById('items');
 
-//by class
-//let allproductNames = document.getElementsByClassName('productName');
-let allproductDescriptions = document.getElementsByClassName('productDescription');
-
-
-    //const links = document.getElementsByTagName('a');
-    // const allProducts = document.getElementById('items');
-    // let clickArticles = document.getElementsByTagName('article'); 
-    // let titleProducts = document.getElementById('title');
-    // const links = document.querySelector('#items a > article');  
-    //       links.addEventListener('click', function (event) {
-    //           let clickArticles = document.getElementsByTagName('article');
-    //           for (let clickArticle of clickArticles)
-    //               clickArticle.addEventListener('click', function (event) {
-    //                   let titleProducts = document.getElementById('title');
-    //                   // let allproductNames = document.getElementsByClassName('productName');
-    //                   titleProducts.innerHTML = event.target.value;
-    //               });
-    //       });
-
-//console.log(allproductNames);
-//console.log(clickArticles);
-//console.log(titleProducts); 
-//console.log(links.childNodes);
-
-// function requestVariables() {
-//     fetch('http://127.0.0.1:5500/front/html/product.html?id=42')
-//         .then(function (res) {
-//             if (res.ok) {
-//                 return res.json();
-//             }
-
-//         })
-//         .then(function (value) {
-//             let titleProducts = document.getElementById('title');
-//             titleProducts.innerText = value.queryString.value;
-//         })
-//         .catch(function (err) {
-            
-//         });
-// }
-//    let allproductNames = document.getElementsByClassName('productName')
-//    for (let allproductName of allproductNames)
-//       allproductName.addEventListener('click', function requestVariables (){
-//            let titleProducts = document.getElementById('title');
-//            titleProducts.innerHTML = requestVariables.target.value;
-//        });
-//console.log(allproductNames); 
-
-
-
-
-function send(e) {
-    e.preventDefault();
-    fetch("http://127.0.0.1:5500/front/html/product.html?id=42", {
-        method: "POST",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ value: document.getElementsByClassName('productName').value })
-    })
+fetch('http://localhost:3000/api/products')
         .then(function (res) {
             if (res.ok) {
-                return res.json();
-            }
-        })
-        .then(function (value) {
-            let allproductNames = document.getElementsByClassName('productName');
-            for (let allproductName of allproductNames)
-                allproductName.addEventListener('change', function(){
-                    allproductName.innerText = value.postData.text;
+                res.json()
+                .then(function(products){
+                    for (let product of products){
+                            allProducts.innerHTML += `<a href="./product.html?id=${product._id}">
+                        <article>
+                            <img src="${product.imageUrl}" alt="${product.altTxt}">
+                            <h3 class="productName">${product.name}</h3> 
+                             <p class="productDescription">${product.description}            
+                        </article>
+                        </a>`
+                    }
+                 console.log(products); 
                 }); 
-            //allproductNames.innerText = value.postData.text;
-            // let titleProducts = document.getElementById('title');
-            // titleProducts.innerText = value.postData.text;
-        });
-}
-let clickArticles = document.getElementsByTagName('article'); 
-for (let clickArticle of clickArticles)
-    clickArticle.addEventListener('click', send); 
+            }
 
-    console.log(clickArticles); 
-    console.log(allproductNames);
+        })
 
-// const links = document.querySelector('#items a > article');
-// links.addEventListener("click", send);
+//<h3 class="${product.name}">Kanap name1</h3>
